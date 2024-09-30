f500c1c92cf9675f935e7962931198a3a3784bd2
Hansi <hansi@schmuckele.de>
2024-09-30 21:09:24 +0200

    ~ scan ui

--- WebApp/Pages/Scan.cshtml.cs
+++ WebApp/Pages/Scan.cshtml.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text;
 
 namespace Albstones.WebApp.Pages;
 
 public class ScanModel : PageModel
 {
+    [BindProperty(SupportsGet = true)]
+    public string? Code { get; set; }
+
+    public bool FoundAlbstone { get; set; }
+
     private readonly ILogger<ScanModel> _logger;
 
     private readonly IConfiguration _config;
@@ -14,7 +21,24 @@ public class ScanModel : PageModel
         _config = config;
     }
 
-    public void OnGet()
+    public async Task<IActionResult> OnGetAsync()
     {
+        if (!string.IsNullOrEmpty(Code))
+        {
+            var json = new StringContent($"{{ \"Scan\": \"{Code}\" }}", Encoding.UTF8, "application/json");
+
+            var baseUri = $"{Request.Scheme}://{Request.Host}";
+
+            using var httpClient = new HttpClient();
+            using var response = await httpClient.PostAsync($"{baseUri}/api/albstones", json);
+            var apiResponse = await response.Content.ReadAsStringAsync();
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                FoundAlbstone = true;
+            }
+        }
+
+        return Page();
     }
 }
